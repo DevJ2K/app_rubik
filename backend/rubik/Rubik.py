ca5e05dedfcfdccbfde82981fc26c97d654c2db4
DevJ2K <ajavontheopro@gmail.com>
2024-09-04 16:32:08 +0200

    ✅ : Can full expend sequences to readable rubik instructions...

--- backend/rubik/Rubik.py
+++ backend/rubik/Rubik.py
@@ -1,4 +1,7 @@
 from visualizers.rubik_visualizer import visualize_cube_3D
+from RubikMoves import RubikMoves
+from ErrorManager import *
+from parser import check_notation
 
 class Rubik:
 	"""The class to solve an rubik cube"""
@@ -46,12 +49,27 @@ class Rubik:
 	def check_valid_cube(self) -> bool:
 		pass
 
+	def apply_move(self, move: RubikMoves) -> None:
+		pass
+
 	def apply_sequences(self, sequences: str) -> None:
 		"""Apply sequences to the rubik cube.
 
 		Args:
 			sequences (str): Sequences to apply to the rubik cube.
 		"""
+		sequences_list_str = sequences.split()
+		for sequence_str in sequences_list_str:
+			if check_notation(sequence_str) == False:
+				raise NotationError(f"{sequence_str} is not a valid notation.")
+
+		sequences_list_not_join = [RubikMoves(notation).sequences for notation in sequences_list_str]
+		sequences_list = []
+		for expend_sequences in sequences_list_not_join:
+			sequences_list += expend_sequences
+		print(sequences_list)
+		for move in sequences_list:
+			self.apply_move(move)
 		pass
 
 	def visualize_cube(self) -> None:
@@ -76,5 +94,5 @@ class Rubik:
 
 if __name__ == "__main__":
 	rubik = Rubik()
-	rubik.visualize_cube()
-	# rubik.apply_sequences("R2 D' B' D F2 R F2 R2 U L' F2 U' B' L2 R D B' R' B2 L2 F2 L2 R2 U2 D2")
+	# rubik.visualize_cube()
+	rubik.apply_sequences("R2 D' B' (RU4R'U')4 D F2 R F2 R2 U L' F2 U' B' L2 R D B' R' B2 L2 	F2 L2 R2 U2 	D2")
