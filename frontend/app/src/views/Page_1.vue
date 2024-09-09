821f7baa16e7ca054bdd0af209dd997fbd2b2a7a
DevJ2K <ajavontheopro@gmail.com>
2024-09-09 18:38:43 +0200

    ✅ : Create all movements of the rubik and cleaning code...

--- frontend/app/src/views/Page_1.vue
+++ frontend/app/src/views/Page_1.vue
@@ -60,29 +60,25 @@ const createCube_ = ({ colors }: { colors?: Array<THREE.MeshBasicMaterial> | und
 
 const createCube = ({
   colors = "white",
-  size = 1, // Valeur par défaut pour size
-  position = { x: 0, y: 0, z: 0 } // Valeur par défaut pour position
+  size = 1,
+  position = { x: 0, y: 0, z: 0 }
 }: {
-  colors?: String; // Optionnel
-  size?: number; // Optionnel
-  position?: { x: number; y: number; z: number }; // Optionnel
+  colors?: String;
+  size?: number;
+  position?: { x: number; y: number; z: number };
 } = {}) => {
-  // Création de la géométrie du cube avec la taille spécifiée
   const geometry = new THREE.BoxGeometry(size, size, size);
 
   const textureLoader = new THREE.TextureLoader();
-  const texture = textureLoader.load(new URL('../assets/rubik_textures/' + colors + '.png', import.meta.url).href); // Charge la texture correctement
-  // console.log(texture)
-  const textureMaterial = new THREE.MeshBasicMaterial({ map: texture }); // Associe la texture au matériau
+  const texture = textureLoader.load(new URL('../assets/rubik_textures/' + colors + '.png', import.meta.url).href);
 
-  // Si colors est défini, on l'utilise, sinon on applique un matériau par défaut
+  const textureMaterial = new THREE.MeshBasicMaterial({ map: texture });
+
   const defaultMaterial = new THREE.MeshBasicMaterial({ color: 0xFF });
   const cube = new THREE.Mesh(geometry, [textureMaterial,textureMaterial,textureMaterial,textureMaterial,textureMaterial,textureMaterial]);
 
-  // Positionnement du cube
   cube.position.set(position.x, position.y, position.z);
 
-  // scene.add(cube);
   return cube;
 }
 
@@ -129,29 +125,94 @@ const createRubik = ({
 
 
 class Rubik3D {
+  // Cube info
   x: number;
   y: number;
   z: number;
   all_cubes: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[], THREE.Object3DEventMap>[];
+
+  // Animation settings
+  second_between_animation: number;
+  animation_speed: number;
   animation_is_playing: boolean;
 
-  tween_group: TWEEN.Group;
-
-  all_tweens: Array<TWEEN.Tween>;
+  // Animation visualization
+  current_frame: number;
+  current_tween: TWEEN.Tween | undefined;
+  frames: Array<Object>
 
   constructor(center_x?: number, center_y?: number, center_z?: number, ) {
     this.x = center_x ?? 0;
     this.y = center_y ?? 0;
     this.z = center_z ?? 0;
 
-    this.tween_group = new TWEEN.Group();
+    this.second_between_animation = 0;
+    this.animation_speed = 1;
 
-    this.all_tweens = [];
+    this.all_cubes = createRubik({center: {x: this.z, y: this.y, z: this.z}});
+    this.display();
 
-    this.all_cubes = createRubik({center: {x: this.z, y: this.y, z: this.z}})
+    this.current_frame = 0;
+    this.frames = [
+      {
+        move: "U"
+      },
+      {
+        move: "U'"
+      },
+      {
+        move: "D"
+      },
+      {
+        move: "D'"
+      },
+      {
+        move: "F"
+      },
+      {
+        move: "F'"
+      },
+      {
+        move: "B"
+      },
+      {
+        move: "B'"
+      },
+      {
+        move: "L"
+      },
+      {
+        move: "L'"
+      },
+      {
+        move: "R"
+      },
+      {
+        move: "R'"
+      },
+    ]
 
     this.animation_is_playing = false;
-    this.display();
+  }
+
+
+  set change_rotation_speed(speed: number) {
+    this.animation_speed = speed;
+  }
+
+  set change_second_between_movements(second: number) {
+    this.second_between_animation = second;
+  }
+
+  async play_animation(): Promise<void> {
+    this.animation_is_playing = true;
+    for (let i = 0; i < this.frames.length; i++) {
+      const element = this.frames[i];
+      console.log(element);
+      await this.apply_moves(element.move);
+
+    }
+    this.current_tween = undefined;
   }
 
   display(): void {
@@ -161,20 +222,20 @@ class Rubik3D {
     }
   }
 
-  play_animation(): void {
-    this.tween_group.removeAll();
-    for (let i = 0; i + 1 < this.all_tweens.length; i++) {
-      const element = this.all_tweens[i];
-      element.chain(this.all_tweens[i + 1]);
+  // play_animation(): void {
+  //   this.tween_group.removeAll();
+  //   for (let i = 0; i + 1 < this.all_tweens.length; i++) {
+  //     const element = this.all_tweens[i];
+  //     element.chain(this.all_tweens[i + 1]);
 
-    }
-    for (let i = 0; i < this.all_tweens.length; i++) {
-      this.tween_group.add(this.all_tweens[i]);
-    }
-    this.animation_is_playing = true;
-  }
+  //   }
+  //   for (let i = 0; i < this.all_tweens.length; i++) {
+  //     this.tween_group.add(this.all_tweens[i]);
+  //   }
+  //   this.animation_is_playing = true;
+  // }
 
-  private animate_rubik(cube_to_move: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[], THREE.Object3DEventMap>[], new_rotation: { x: number; y: number; z: number }) {
+  private async animate_rubik(cube_to_move: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[], THREE.Object3DEventMap>[], new_rotation: { x: number; y: number; z: number }): Promise<void> {
     console.log("Animating...")
     const group = new THREE.Group();
     for (let i = 0; i < cube_to_move.length; i++) {
@@ -182,51 +243,105 @@ class Rubik3D {
       group.add(cube)
     }
 
-    console.log(group.rotation)
-    // group.rotation.y = THREE.MathUtils.degToRad(90);
-
     scene.add(group)
 
+    // console.log(group.rotation);
+    // await new Promise<void>((resolve, reject) => {
+    //   setTimeout(resolve, 1000);
+    // })
+
     const targetRotation = {
-      x: THREE.MathUtils.degToRad(new_rotation.x),
+      x: THREE.MathUtils.degToRad(group.rotation.x + new_rotation.x),
       y: THREE.MathUtils.degToRad(group.rotation.y + new_rotation.y),
-      z: THREE.MathUtils.degToRad(new_rotation.z)
+      z: THREE.MathUtils.degToRad(group.rotation.z + new_rotation.z)
     };
 
-    const tween = new TWEEN.Tween(group.rotation)
-      .to(targetRotation, 2000)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .onUpdate(() => {
-        // Optionnel : Si vous voulez faire quelque chose pendant l'animation
-        console.log("Here TWEEN");
-      })
-      .onComplete(() => {
-        console.log(group.rotation)
-        console.log("Rotation complete");
-      })
-      .start()
+    return new Promise((resolve) => {
+      this.current_tween = new TWEEN.Tween(group.rotation)
+        .to(targetRotation, 1000 * this.animation_speed)
+        .easing(TWEEN.Easing.Cubic.InOut)
+        .onUpdate(() => {})
+        .onComplete(async () => {
+          console.log("Rotation complete");
+        for (let i = 0; i < cube_to_move.length; i++) {
+          const cube = cube_to_move[i];
+          cube.material[2].color.setHex(0xffffff);
+          scene.attach(cube);
+        }
+        scene.remove(group);
 
-      // tween.update()
-      this.all_tweens.push(tween);
-      // this.tween_group.add(tween);
+        await new Promise<void>((resolve) => {
+          setTimeout(resolve, 1000 * this.second_between_animation);
+        })
+
+        resolve();
+        })
+        .start();
+    });
   }
 
-  apply_moves(move: string): void {
+  private async apply_moves(move: string): Promise<void> {
     const cube_to_move: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[], THREE.Object3DEventMap>[] = [];
-    console.log(("=".repeat(20)) + " " + move + " " + ("=".repeat(20)));
-    if (move === "U") {
-      for (let i = 0; i < this.all_cubes.length; i++) {
-        const cube = this.all_cubes[i];
-        if (cube.position.y > 0) {
-          cube.material[2].color.setHex(0x00ffff);
-          cube_to_move.push(cube);
-          // console.log(cube.position);
-        }
 
+    let x: number = 0;
+    let y: number = 0;
+    let z: number = 0;
+
+
+
+    for (let i = 0; i < this.all_cubes.length; i++) {
+      const cube = this.all_cubes[i];
+
+      if (move === "U" || move === "U'") {
+        y = move === "U" ? 90 : -90
+        if (cube.position.y > 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
       }
-      this.animate_rubik(cube_to_move, {x: 0, y: -90, z: 0});
-      // console.log("HERE")
+      else if (move === "D" || move === "D'") {
+        y = move === "D" ? 90 : -90
+        if (cube.position.y < 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
+      }
+      else if (move === "F" || move === "F'") {
+        z = move === "F" ? -90 : 90
+        if (cube.position.z > 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
+      }
+      else if (move === "B" || move === "B'") {
+        z = move === "B" ? 90 : -90
+        if (cube.position.z < 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
+      }
+      else if (move === "L" || move === "L'") {
+        x = move === "L" ? 90 : -90
+        if (cube.position.x < 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
+      }
+      else if (move === "R" || move === "R'") {
+        x = move === "R" ? -90 : 90
+        if (cube.position.x > 0) {
+            cube.material[2].color.setHex(0x00ffff);
+            cube_to_move.push(cube);
+        }
+      }
+      else {
+        alert("Invalid movement detected !")
+        console.log("Invalid movement detected !");
+      }
     }
+
+
+    await this.animate_rubik(cube_to_move, {x: x, y: y, z: z});
   }
 }
 
@@ -237,11 +352,8 @@ const initThree = () => {
   // let all_cubes = createRubik();
 
   let rubik3D = new Rubik3D();
-  rubik3D.apply_moves("U");
-  rubik3D.apply_moves("U");
-  rubik3D.apply_moves("U");
   rubik3D.play_animation();
-  // rubik3D.display()
+  // // rubik3D.display()
 
   const animate = (time: number) => {
     // line.rotation.x += 0.01;
@@ -249,8 +361,8 @@ const initThree = () => {
     // cube.rotation.x += 0.01;
     // cube.rotation.y += 0.01;
     controls.update();
-    if (rubik3D.animation_is_playing) {
-      rubik3D.tween_group.update()
+    if (rubik3D.animation_is_playing && rubik3D.current_tween) {
+      rubik3D.current_tween.update();
     }
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
@@ -260,73 +372,6 @@ const initThree = () => {
   // animate();
 
 
-}
-
-const initThree_ = () => {
-  if (threeContainer.value == null) {
-    return
-  }
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x404040);
-
-  // Utiliser la référence définie
-  threeContainer.value.appendChild(renderer.domElement);
-
-  // const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-
-
-  const controls = new OrbitControls( camera, renderer.domElement );
-
-  const group = new THREE.Group();
-
-//   const material = new THREE.ShaderMaterial({
-//   uniforms: {
-//     thickness: {
-//       value: 1.5
-//     }
-//   },
-//   // vertexShader: vertexShader,
-//   // fragmentShader: fragmentShader
-// });
-
-
-  camera.position.z = 5;
-
-
-  const geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
-  const edges = new THREE.EdgesGeometry( geometry );
-  const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( {
-    color: 0xffffff,
-    linewidth: 15,
-   } ) );
-
-
-  scene.add( line );
-
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  line.rotation.x = 45
-
-  line.rotation.set(45, 45, 0);
-  cube.rotation.set(45, 45, 0);
-
-  const animate = () => {
-    requestAnimationFrame(animate);
-    // line.rotation.x += 0.01;
-    // line.rotation.y += 0.01;
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    controls.update();
-    renderer.render(scene, camera);
-  };
-
-  animate();
 }
 
 onMounted(() => {
