1888b35fb8069b28837000d56f2bf068b6d15154
DevJ2K <ajavontheopro@gmail.com>
2024-09-18 10:19:19 +0200

    ✅ : Responsive in the first page...

--- frontend/app/src/views/HomeView.vue
+++ frontend/app/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class=" container flex h-screen w-full flex-col items-center justify-center gap-24 text-high-contrast-text dark:text-d-high-contrast-text">
-    <h1 class=" text-5xl font-black">Ultimate Rubik's Cube Solver</h1>
+  <main class=" container flex min-h-screen w-full flex-col items-center justify-center gap-16 text-high-contrast-text dark:text-d-high-contrast-text md:gap-24">
+    <h1 class=" text-center text-2xl font-black sm:text-3xl md:text-5xl">Ultimate Rubik's Cube Solver</h1>
 
-    <div class="flex w-full flex-row items-center justify-around">
+    <div class="flex w-full flex-col items-center justify-around gap-8 md:flex-row md:gap-0">
 
       <!-- Colors Palette -->
-      <div class="flex w-1/6 flex-col items-center justify-center gap-4">
+      <div class="flex flex-row items-center justify-center gap-4 max-md:order-2 md:w-1/6 md:flex-col">
         <div class=" color-choose bg-white"></div>
         <div class=" color-choose bg-red-500"></div>
         <div class=" color-choose bg-orange-400"></div>
@@ -16,41 +16,42 @@
 
       <!-- Canvas -->
       <!-- <div class="w-1/3 "> -->
-        <div class=" size-96 bg-black/20">
+        <div class=" size-96 bg-black/20 max-md:order-1">
         </div>
       <!-- </div> -->
 
       <!-- Movements -->
-       <div class="flex w-1/6 justify-center">
-         <div class="grid w-fit grid-cols-2 gap-4">
-           <div class="movements">U</div>
-           <div class="movements">U'</div>
-           <div class="movements">D</div>
-           <div class="movements">D'</div>
-           <div class="movements">F</div>
-           <div class="movements">F'</div>
-           <div class="movements">B</div>
-           <div class="movements">B'</div>
-           <div class="movements">L</div>
-           <div class="movements">L'</div>
-           <div class="movements">R</div>
-           <div class="movements">R'</div>
-         </div>
-       </div>
+      <div class="flex justify-center max-md:order-3">
+        <div class="grid w-fit grid-cols-6 gap-4 md:grid-cols-2">
+          <div class="movements">U</div>
+          <div class="movements">U'</div>
+          <div class="movements">D</div>
+          <div class="movements">D'</div>
+          <div class="movements">F</div>
+          <div class="movements">F'</div>
+          <div class="movements">B</div>
+          <div class="movements">B'</div>
+          <div class="movements">L</div>
+          <div class="movements">L'</div>
+          <div class="movements">R</div>
+          <div class="movements">R'</div>
+        </div>
+      </div>
     </div>
 
 
+
     <div class="flex flex-col gap-8">
-      <div class="flex flex-row justify-center gap-6">
+      <div class="hidden flex-row justify-center gap-6 sm:flex">
         <input type="text" placeholder="Enter a sequence" class="sequence-input">
         <div class="icon-button"><ClipboardIcon/></div>
         <div class="icon-button"><PlayIcon/></div>
       </div>
-      <div class="flex w-full flex-row justify-between">
+      <div class="grid w-full grid-cols-3 flex-row justify-between max-sm:gap-6 sm:flex">
         <div class="icon-button"><DeleteIcon/></div>
         <div class="icon-button"><AutorenewIcon/></div>
         <div class="icon-button"><ShuffleIcon/></div>
-        <div class="text-button">SOLVE</div>
+        <div class="text-button col-span-3 py-4">SOLVE</div>
       </div>
     </div>
   </main>
