0e9e1a0cc4994052c2597f8336d9e3ba26b3c7e1
theo <ajavontheopro@gmail.com>
2024-09-25 00:00:27 +0200

    💭 : Add short description to RubikCorner.py

--- backend/rubik/RubikCorner.py
+++ backend/rubik/RubikCorner.py
@@ -1,3 +1,4 @@
+# Will be a class who takes a corner has arguments and final position and returns movements to apply and distances.
 class RubikCorner:
 	# 0: urf, 1: ubr, 2: dlf, 3: dfr, 4: ulb, 5: ufl, 6: drb, 7: dbl
 	def __init__(self) -> None:
