9e514eaf952ed16ab35557f2631517bc574993e8
DevJ2K <ajavontheopro@gmail.com>
2024-09-24 12:17:54 +0200

    ✅ : Update position depending RubikSolved default

--- backend/rubik/RubikCustomUtils.py
+++ backend/rubik/RubikCustomUtils.py
@@ -17,3 +17,14 @@ def update_corners_pos(rubikSolved: Rubik, rubik: Rubik):
 			if (same_cube(rubikSolvedCorners[i], rubikCorners[j])):
 				new_corners_pos[j] = i
 	return new_corners_pos
+
+def update_edges_pos(rubikSolved: Rubik, rubik: Rubik):
+	new_edges_pos = rubik.edgePos
+
+	rubikSolvedEdges = rubikSolved.getEdges()
+	rubikEdges = rubik.getEdges()
+	for i in range(len(rubikSolvedEdges)):
+		for j in range(len(rubikEdges)):
+			if (same_cube(rubikSolvedEdges[i], rubikEdges[j])):
+				new_edges_pos[j] = i
+	return new_edges_pos
