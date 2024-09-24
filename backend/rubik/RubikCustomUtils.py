f0af0d9b8ec21ada0030d8c1113fe2f25a2f6d6e
DevJ2K <ajavontheopro@gmail.com>
2024-09-24 12:09:08 +0200

    ✅ : Update position depending RubikSolved default

---
+++ backend/rubik/RubikCustomUtils.py
@@ -0,0 +1,19 @@
+from Rubik import Rubik
+
+def same_cube(cube1: tuple, cube2: tuple):
+	return sorted(cube1) == sorted(cube2)
+
+
+def update_corners_pos(rubikSolved: Rubik, rubik: Rubik):
+	new_corners_pos = rubik.cornerPos
+
+	rubikSolvedCorners = rubikSolved.getCorners()
+	rubikCorners = rubik.getCorners()
+	for i in range(len(rubikSolvedCorners)):
+		for j in range(len(rubikCorners)):
+			# print(rubikSolvedCorners[i])
+			# print(cube)
+			# print(same_cube(rubikSolvedCorners[i], cube))
+			if (same_cube(rubikSolvedCorners[i], rubikCorners[j])):
+				new_corners_pos[j] = i
+	return new_corners_pos
